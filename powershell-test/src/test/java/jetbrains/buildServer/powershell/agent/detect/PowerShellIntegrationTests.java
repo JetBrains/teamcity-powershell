d75502f0fbaae742c89185151bad832dbe0df429
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-06-25 12:52:25 +0000

    cover most popular use-cases with tests

--- powershell-test/src/test/java/jetbrains/buildServer/powershell/agent/detect/PowerShellIntegrationTests.java
+++ powershell-test/src/test/java/jetbrains/buildServer/powershell/agent/detect/PowerShellIntegrationTests.java
@@ -1,13 +1,19 @@
 package jetbrains.buildServer.powershell.agent.detect;
 
 import jetbrains.buildServer.RunnerTest2Base;
-import jetbrains.buildServer.powershell.common.*;
+import jetbrains.buildServer.powershell.common.PowerShellBitness;
+import jetbrains.buildServer.powershell.common.PowerShellConstants;
+import jetbrains.buildServer.powershell.common.PowerShellExecutionMode;
+import jetbrains.buildServer.powershell.common.PowerShellScriptMode;
 import jetbrains.buildServer.serverSide.SFinishedBuild;
+import jetbrains.buildServer.util.FileUtil;
 import jetbrains.buildServer.util.TestFor;
 import junit.framework.Assert;
 import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 /**
  * Created 18.06.13 12:59
@@ -35,10 +41,54 @@ public class PowerShellIntegrationTests extends RunnerTest2Base {
 
   @Test
   @TestFor(issues = "TW-29803")
-  public void should_run_simple_command() throws Throwable {
+  public void should_run_simple_command_code_stdin() throws Throwable {
     setRunnerParameter(PowerShellConstants.RUNNER_EXECUTION_MODE, PowerShellExecutionMode.STDIN.getValue());
     setRunnerParameter(PowerShellConstants.RUNNER_SCRIPT_MODE, PowerShellScriptMode.CODE.getValue());
     setRunnerParameter(PowerShellConstants.RUNNER_SCRIPT_CODE, "echo works");
+    setRunnerParameter(PowerShellConstants.RUNNER_BITNESS, PowerShellBitness.x86.getValue());
+
+    final SFinishedBuild build = doTest(null);
+    dumpBuildLogLocally(build);
+    Assert.assertTrue(build.getBuildStatus().isSuccessful());
+  }
+
+  @Test
+  @TestFor(issues = "TW-29803")
+  public void should_run_simple_command_file_stdin() throws Throwable {
+    final File code = createTempFile("echo works");
+    setRunnerParameter(PowerShellConstants.RUNNER_EXECUTION_MODE, PowerShellExecutionMode.STDIN.getValue());
+    setRunnerParameter(PowerShellConstants.RUNNER_SCRIPT_MODE, PowerShellScriptMode.FILE.getValue());
+    setRunnerParameter(PowerShellConstants.RUNNER_SCRIPT_FILE, code.getPath());
+    setRunnerParameter(PowerShellConstants.RUNNER_BITNESS, PowerShellBitness.x86.getValue());
+
+    final SFinishedBuild build = doTest(null);
+    dumpBuildLogLocally(build);
+    Assert.assertTrue(build.getBuildStatus().isSuccessful());
+  }
+
+  @Test
+  @TestFor(issues = "TW-29803")
+  public void should_run_simple_command_code_ps1() throws Throwable {
+    setRunnerParameter(PowerShellConstants.RUNNER_EXECUTION_MODE, PowerShellExecutionMode.PS1.getValue());
+    setRunnerParameter(PowerShellConstants.RUNNER_SCRIPT_MODE, PowerShellScriptMode.CODE.getValue());
+    setRunnerParameter(PowerShellConstants.RUNNER_SCRIPT_CODE, "echo works");
+    setRunnerParameter(PowerShellConstants.RUNNER_BITNESS, PowerShellBitness.x86.getValue());
+
+    final SFinishedBuild build = doTest(null);
+    dumpBuildLogLocally(build);
+    Assert.assertTrue(build.getBuildStatus().isSuccessful());
+  }
+
+  @Test
+  @TestFor(issues = "TW-29803")
+  public void should_run_simple_command_file_ps1() throws Throwable {
+    final File dir = createTempDir();
+    final File code = new File(dir, "code.ps1");
+    FileUtil.writeFileAndReportErrors(code, "echo works");
+
+    setRunnerParameter(PowerShellConstants.RUNNER_EXECUTION_MODE, PowerShellExecutionMode.PS1.getValue());
+    setRunnerParameter(PowerShellConstants.RUNNER_SCRIPT_MODE, PowerShellScriptMode.FILE.getValue());
+    setRunnerParameter(PowerShellConstants.RUNNER_SCRIPT_FILE, code.getPath());
     setRunnerParameter(PowerShellConstants.RUNNER_BITNESS, PowerShellBitness.x86.getValue());
 
     final SFinishedBuild build = doTest(null);
