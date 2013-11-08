d30199480236a7ce2f28e01430a3e6914b0a708e
Pavel Sher <pavel.sher@gmail.com>
2013-11-08 08:12:52 +0000

    fixing compilation

--- powershell-server/src/main/java/jetbrains/buildServer/powershell/server/PowerShellRunnerDiscoverer.java
+++ powershell-server/src/main/java/jetbrains/buildServer/powershell/server/PowerShellRunnerDiscoverer.java
@@ -5,8 +5,8 @@ import jetbrains.buildServer.powershell.common.PowerShellBitness;
 import jetbrains.buildServer.powershell.common.PowerShellConstants;
 import jetbrains.buildServer.powershell.common.PowerShellExecutionMode;
 import jetbrains.buildServer.powershell.common.PowerShellScriptMode;
-import jetbrains.buildServer.serverSide.BreadthFirstRunnerDiscoveryExtension;
-import jetbrains.buildServer.serverSide.DiscoveredBuildRunner;
+import jetbrains.buildServer.serverSide.discovery.BreadthFirstRunnerDiscoveryExtension;
+import jetbrains.buildServer.serverSide.discovery.DiscoveredObject;
 import jetbrains.buildServer.util.browser.Element;
 import org.jetbrains.annotations.NotNull;
 
@@ -28,7 +28,7 @@ public class PowerShellRunnerDiscoverer extends BreadthFirstRunnerDiscoveryExten
   @NotNull
   @Override
   protected List<DiscoveredBuildRunner> discoverRunnersInDirectory(@NotNull final Element dir, @NotNull final List<Element> filesAndDirs) {
-    final List<DiscoveredBuildRunner> runners = new ArrayList<DiscoveredBuildRunner>();
+    final List<DiscoveredObject> runners = new ArrayList<DiscoveredObject>();
     for (Element e: filesAndDirs) {
       if (e.isLeaf() && PS_EXT.equals(FileUtil.getExtension(e.getName()))) {
         final Map<String, String> parameters = new HashMap<String, String>();
@@ -36,7 +36,7 @@ public class PowerShellRunnerDiscoverer extends BreadthFirstRunnerDiscoveryExten
         parameters.put(PowerShellConstants.RUNNER_BITNESS, PowerShellBitness.values()[0].getValue());
         parameters.put(PowerShellConstants.RUNNER_EXECUTION_MODE, PowerShellExecutionMode.STDIN.getValue());
         parameters.put(PowerShellConstants.RUNNER_SCRIPT_MODE, PowerShellScriptMode.FILE.getValue());
-        runners.add(new DiscoveredBuildRunner(PowerShellConstants.RUN_TYPE, parameters, PowerShellConstants.RUN_TYPE + " " + e.getName()));
+        runners.add(new DiscoveredObject(PowerShellConstants.RUN_TYPE, parameters, PowerShellConstants.RUN_TYPE + " " + e.getName()));
       }
     }
     return runners;
