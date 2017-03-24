cbdf1ee90099af0c664cfa0b499204dd25d8a008
Oleg Rybak <oleg.rybak@gmail.com>
2017-03-24 13:57:21 +0000

    TW-49208 Better message

--- powershell-agent/src/main/java/jetbrains/buildServer/powershell/agent/ScriptGenerator.java
+++ powershell-agent/src/main/java/jetbrains/buildServer/powershell/agent/ScriptGenerator.java
@@ -74,7 +74,8 @@ public class ScriptGenerator {
       scriptFile = writeToTempFile(buildTempDir, sourceScript, runnerParameters);
     }
     if (!scriptFile.isFile()) {
-      throw new RunBuildException("PowerShell script [" + scriptFile.getAbsolutePath() + "] does not exist");
+      throw new RunBuildException("Cannot find PowerShell script by path specified in build configuration settings:"
+          + scriptFile.getAbsolutePath() + "] (absolute path on agent). Please check that specified path is correct.");
     }
     return scriptFile;
   }
