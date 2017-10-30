0128723ddbefe3b12a43420d2b942949c8eee5d3
Oleg.Rybak <oleg.rybak@gmail.com>
2017-10-30 17:49:34 +0300

    TW-52144 Cannot detect PowerShell build runner on TeamCity agent Added support for pwsh executable
    (cherry picked from commit 5c61f65)

--- powershell-agent/src/main/java/jetbrains/buildServer/powershell/agent/detect/cmd/CommandLinePowerShellDetector.java
+++ powershell-agent/src/main/java/jetbrains/buildServer/powershell/agent/detect/cmd/CommandLinePowerShellDetector.java
@@ -42,18 +42,21 @@ public class CommandLinePowerShellDetector implements PowerShellDetector {
   @NotNull
   private final DetectionRunner myRunner;
 
-  private List<String> paths = Arrays.asList(
-      "/usr/local/bin", // mac os
-      "/usr/bin"        // linux
+  private static final List<String> PATHS = Arrays.asList(
+          "/usr/local/bin", // mac os
+          "/usr/bin"        // linux
   );
 
-  private static final String EXECUTABLE = "powershell";
+  private static final List<String> EXECUTABLES = Arrays.asList(
+          "pwsh",
+          "powershell"
+  );
 
   private static final String DETECTION_SCRIPT =
-      "Write-Output " +
-          "$PSVersionTable.PSVersion.toString() " + // shell version
-          "$PSVersionTable.PSEdition.toString() " + // shell edition
-          "([IntPtr]::size -eq 8)";                 // shell bitness
+          "Write-Output " +
+                  "$PSVersionTable.PSVersion.toString() " + // shell version
+                  "$PSVersionTable.PSEdition.toString() " + // shell edition
+                  "([IntPtr]::size -eq 8)";                 // shell bitness
 
   public CommandLinePowerShellDetector(@NotNull final BuildAgentConfiguration configuration,
                                        @NotNull final DetectionRunner runner) {
@@ -79,27 +82,27 @@ public class CommandLinePowerShellDetector implements PowerShellDetector {
           // what if 32 bit installation is in place of 64 bit? or otherwise?
           // need to override in constructor of PowerShellInfo
           for (Map.Entry<PowerShellBitness, String> e: detectionContext.getPredefinedPaths().entrySet()) {
-            File executableLocation = new File(e.getValue(), EXECUTABLE);
-            PowerShellInfo info = doDetect(executableLocation, scriptPath);
-            if (info != null) {
-              if (info.getBitness() != e.getKey()) { // if we are to substitute PowerShell installation explicitly, ignoring the bits of detected one
-                LOG.warn("Using configured bitness (" + e.getKey() + ") for PowerShell at [" + info.getHome() + "] instead of detected one (" + info.getBitness() + ")");
+            for (String executable : EXECUTABLES) {
+              PowerShellInfo info = doDetect(e.getValue(), executable, scriptPath);
+              if (info != null) {
+                if (info.getBitness() != e.getKey()) { // if we are to substitute PowerShell installation explicitly, ignoring the bits of detected one
+                  LOG.warn("Using configured bitness (" + e.getKey() + ") for PowerShell at [" + info.getHome() + "] instead of detected one (" + info.getBitness() + ")");
+                }
+                register(result, new PowerShellInfo(e.getKey(), info.getHome(), info.getVersion(), info.getEdition(), info.getExecutable()));
               }
-              register(result, new PowerShellInfo(e.getKey(), info.getHome(), info.getVersion(), info.getEdition()));
             }
           }
         }
 
-        final List<String> pathsToCheck = !detectionContext.getSearchPaths().isEmpty() ? detectionContext.getSearchPaths() : paths;
+        final List<String> pathsToCheck = !detectionContext.getSearchPaths().isEmpty() ? detectionContext.getSearchPaths() : PATHS;
         for (String path: pathsToCheck) {
-          File executableLocation = new File(path, EXECUTABLE);
-          if (executableLocation.isFile()) {
-            final PowerShellInfo detected = doDetect(executableLocation, scriptPath);
+          for (String executable: EXECUTABLES) {
+            final PowerShellInfo detected = doDetect(path, executable, scriptPath);
             if (detected != null) {
               if (result.get(detected.getBitness()) != null) {
                 LOG.warn("Ignoring " + detected.getBitness() + " PowerShell at " + detected.getHome()
-                    + " as it is explicitly replaced by " + result.get(detected.getBitness()).getHome()
-                    + " in build agent properties");
+                        + " as it is explicitly replaced by " + result.get(detected.getBitness()).getHome()
+                        + " in build agent properties");
               } else {
                 register(result, detected);
               }
@@ -120,26 +123,37 @@ public class CommandLinePowerShellDetector implements PowerShellDetector {
   }
 
   @Nullable
-  private PowerShellInfo doDetect(@NotNull final File executable,
+  private PowerShellInfo doDetect(@NotNull final String homePath,
+                                  @NotNull final String executable,
                                   @NotNull final String scriptPath) {
     PowerShellInfo result = null;
-    final String executablePath = executable.getAbsolutePath();
-    try {
-      final List<String> outputLines = myRunner.runDetectionScript(executablePath, scriptPath);
-      if (outputLines.size() == 3) {
-        final PowerShellEdition edition = PowerShellEdition.fromString(outputLines.get(1));
-        if (edition != null) {
-          result = new PowerShellInfo(Boolean.valueOf(outputLines.get(2)) ? PowerShellBitness.x64 : PowerShellBitness.x86, executable.getParentFile(), outputLines.get(0), edition);
+    final File exeFile = new File(homePath, executable);
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("Searching for PowerShell at " + exeFile.getAbsolutePath());
+    }
+    if (exeFile.isFile()) {
+      String executablePath = exeFile.getAbsolutePath();
+      try {
+        final List<String> outputLines = myRunner.runDetectionScript(executablePath, scriptPath);
+        if (outputLines.size() == 3) {
+          final PowerShellEdition edition = PowerShellEdition.fromString(outputLines.get(1));
+          if (edition != null) {
+            result = new PowerShellInfo(Boolean.valueOf(outputLines.get(2)) ? PowerShellBitness.x64 : PowerShellBitness.x86, exeFile.getParentFile(), outputLines.get(0), edition, executable);
+          } else {
+            LOG.warn("Failed to determine PowerShell edition for [" + executablePath + "]");
+            LOG.debug(StringUtil.join("\n", outputLines));
+          }
         } else {
-          LOG.warn("Failed to determine PowerShell edition for [" + executablePath + "]");
+          LOG.warn("Failed to parse output from PowerShell executable [" + executablePath + "]");
           LOG.debug(StringUtil.join("\n", outputLines));
         }
-      } else {
-        LOG.warn("Failed to parse output from PowerShell executable [" + executablePath + "]");
-        LOG.debug(StringUtil.join("\n", outputLines));
+      } catch (ExecutionException e) {
+        LOG.warnAndDebugDetails("Failed to run PowerShell detection script [" + scriptPath + "] with executable [" + executablePath + "]", e);
       }
-    } catch (ExecutionException e) {
-      LOG.warnAndDebugDetails("Failed to run PowerShell detection script [" + scriptPath + "] with executable [" + executablePath + "]", e);
+    } else {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("PowerShell at " + exeFile.getAbsolutePath() + " was not found");
+      }
     }
     return result;
   }
