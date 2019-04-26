c1b6a0627e511f2ba3f5ce33845e5f09b1de0502
Oleg Rybak <oleg.rybak@jetbrains.com>
2019-04-26 15:08:30 +0300

    TW-60102 The latest PowerShell (6.2.0) isn't detected on MacOS New macOS installers (brew cask install ...) do not remove old beta versions that are installed from pkg directly. This causes old versions (with outdated executable name) to be detected Now legacy powershell will only be detected if there are no release versions available

--- powershell-test-server/src/test/java/jetbrains/buildServer/powershell/server/PowerShellRunTypeTest.java
+++ powershell-test-server/src/test/java/jetbrains/buildServer/powershell/server/PowerShellRunTypeTest.java
@@ -226,6 +226,6 @@ public class PowerShellRunTypeTest extends BaseTestCase {
   }
 
   private String[] sampleVersions() {
-    return new String[] {"1.0", "2.0", "3.0", "4.0", "5.0", "5.1", "6.0"};
+    return new String[] {"1.0", "2.0", "3.0", "4.0", "5.0", "5.1", "5.1.17763.1", "6.0", "6.1.0"};
   }
 }
